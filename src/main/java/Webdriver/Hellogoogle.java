1df2e1a75e1265b51e57c8848555e9f13d751c9d
Snigdha Susana Chandra Pathroju <sscpathroju@adaequare.com>
2022-02-18 15:43:27 +0530

    firstcommit

---
+++ src/main/java/Webdriver/Hellogoogle.java
@@ -0,0 +1,27 @@
+package Webdriver;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Hellogoogle {
+	
+	@Test
+	
+public void TestBrowser() throws Exception{
+		
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver= new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.google.com/");
+		driver.findElement(By.name("q")).sendKeys("Java",Keys.ENTER);
+		System.out.println(driver.getTitle());
+		Thread.sleep(5000);
+		driver.quit();
+
+}
+}
